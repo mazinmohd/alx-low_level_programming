5428623af6909c145c0057a2cda48fb6069ada0d
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-10 12:27:41 +0300

    seventh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  * main - Entry point
+  *
+  * Description: progam print text 
+  *
+  * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
