9fa7e21ec9d4e7bc7077f4ac32e452be75246861
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-10 12:05:22 +0300

    first

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, que, 59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - print text
+  *
+  * Description : use puts function to print text
+  *
+  * Retrun: Alawys 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
