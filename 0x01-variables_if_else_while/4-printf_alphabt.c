8bd7e52f8b03d3eab66cff20992e074ddc7614d1
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-11 15:41:27 +0300

    fif

---
+++ 0x01-variables_if_else_while/4-printf_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: program print all most alphabt
+  *
+  * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
