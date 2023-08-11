a8c62cc648f88c807c22c14dc4fc049ae08df8ec
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-11 16:06:12 +0300

    nine

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = '0';
+	char b = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
