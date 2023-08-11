96bc739004473a42aa17476e23af8d989d659c0b
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-11 15:30:06 +0300

    forth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: program pritn all capital and small alphabet
+  *
+  * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
