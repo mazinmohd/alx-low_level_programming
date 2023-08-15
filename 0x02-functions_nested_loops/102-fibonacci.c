d74f007613324e40b44e2d1a6aaf230b2b29a584
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 21:34:28 +0300

    l

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	unsigned long fb1 = 0, fb2 = 0, s;
+
+	for (c = 0; c < 50; c++)
+	{
+		s = fb1 + fb2;
+		printf("%lu", s);
+
+		fb1 = fb2;
+		fb2 = s;
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+
+	}
+	return (0);
+}
