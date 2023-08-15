e66500f6e078e1e61f8236036e0d732ef7fdde79
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 19:40:30 +0300

    ten

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,27 @@
 
 void times_table(void)
 {
-	int n = 0, b;
+	int n = 0, b, x;
 
 	while (n <= 9)
 	{
-		b = 0;
+		_putchar(48);
+		b = 1;
 		while (b <= 9)
 		{
-			_putchar((n + 48) * (b + 48));
 			_putchar(',');
 			_putchar(' ');
+
+			x = n * b;
+
+			if (x <= 9)
+				_putchar(' ');
+			else
+				_putchar((x / 10) + 48);
+			_putchar((x % 10) + 48);
 			b++;
 		}
+		putchar('\n');
 		n++;
 	}
 }
