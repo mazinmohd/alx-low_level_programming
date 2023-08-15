3f995b2c47408733f6768bad69efb2a30a6afcf8
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 13:28:12 +0300

    third

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 0; a < 11; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 'a'; b <= 'z'; b++)
 			_putchar(b);
-		putchar('\n');
+		_putchar('\n');
 	}
 }
