ae8723f2c31cab30447964a98ae0c3e459b20f50
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 20:04:24 +0300

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,10 @@ void print_to_98(int n)
 	int a;
 
 	if (n > 98)
-	{
 		for (a = n; a < 98; a--)
 			printf("%d, ", a);
-	}
 	else
-	{
 		for (a = n; a > 98; a++)
 			printf("%d, ", a);
-	}
 	printf("98\n");
 }
