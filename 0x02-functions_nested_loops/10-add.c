4522dbfac7ebf38b914cfc77db4cc7293f0bff64
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 19:49:14 +0300

    eleven

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * add - add two integrs to each other
+  *
+  *@a: first input
+  *
+  *@b: second input
+  *
+  *Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int rus;
+
+	rus = a + b;
+	return (rus);
+}
