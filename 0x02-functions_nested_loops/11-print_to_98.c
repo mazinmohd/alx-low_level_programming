2212cf6f14ee0462ffa1569656ef7b9d4b6c9ea9
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-15 20:00:22 +0300

    last

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_to_98 - print all natural number
+  *
+  * @n: input value
+ */
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n > 98)
+	{
+		for (a = n; a < 98; a--)
+			printf("%d, ", a);
+	}
+	else
+	{
+		for (a = n; a > 98; a++)
+			printf("%d, ", a);
+	}
+	printf("98\n");
+}
