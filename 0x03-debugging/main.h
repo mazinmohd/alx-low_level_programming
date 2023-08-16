88a8134e5a5f5e555abad8b99f48f5b4c8cd690e
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-16 08:54:01 +0300

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,5 +9,5 @@ int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
-void print_remainig_day(int month, int day, int year);
+void print_remaining_day(int month, int day, int year);
 #endif /* MAIN_H */
