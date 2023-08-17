f61d7b1bce04f477f5e0200a35a2af753ea43d64
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-17 12:09:28 +0300

    print more numver

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,9 @@ void print_most_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		if (a != 2 || a != 4)
-			_putchar(a + '0');
+		if (a == 2 || a == 4)
+			continue;
+		_putchar(a + '0');
 	}
 	_putchar('\n');
 }
