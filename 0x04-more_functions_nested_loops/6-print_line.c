3b7a3b78bdf56951f6f9197a4ad380157caf266a
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-17 12:12:08 +0300

    betty alabeta

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,6 +7,7 @@
 void print_line(int n)
 {
 	int a;
+
 	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 void print_diagonal(int n)
 {
 	int a;
-	
+
 	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
