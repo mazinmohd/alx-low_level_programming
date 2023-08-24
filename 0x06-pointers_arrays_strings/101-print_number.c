3676df616bc8624669347ed59cd64404bb48c8a7
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-24 15:25:58 +0300

    adv

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_number - print an integer
+  * @n: input value
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + 48);
+}
