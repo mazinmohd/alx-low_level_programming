02d182f11b178aebfc5f6922f468db1a7b5d94b3
mazinmohd <mazenomermazen2002@gmail.com>
2023-08-30 12:15:36 +0300

    third

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,21 +20,4 @@ int _sqrt_recursion(int n)
 	return (squar(n, 1));
 }
 
-int main(void)
-{
-    int r;
 
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
-}
