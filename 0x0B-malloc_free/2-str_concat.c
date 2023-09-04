fdd20ab034c29f0b3dd55115de847a217831ec15
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-04 09:07:15 +0300

    third

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,11 +20,13 @@ char *str_concat(char *s1, char *s2)
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
 	m = malloc((size1 + size2) * sizeof(char) + 1);
+	if (m == NULL)
+        {
+            printf("Can't allocate %d bytes (after %d calls)\n", INT_MAX, i);
 	for (i = 0; i < size1; i++)
 		m[i] = s1[i];
 	for (j = 0; j < size2; j++)
 		m[i + j] = s2[j];
 	m[i + j] = '\0';
-	free(m);
 	return (m);
 }
