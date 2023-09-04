3a3f0e477ce7ef07043a26a903648bc774ffa7b3
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-04 09:09:19 +0300

    third

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
   * *str_concat - concatenates two string
   * @s1: input
@@ -23,6 +24,7 @@ char *str_concat(char *s1, char *s2)
 	if (m == NULL)
         {
             printf("Can't allocate %d bytes (after %d calls)\n", INT_MAX, i);
+	}
 	for (i = 0; i < size1; i++)
 		m[i] = s1[i];
 	for (j = 0; j < size2; j++)
