a61ba24db088e06aab2f6cb0c964b599d6e135c4
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-13 14:02:37 +0300

    first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
   * print_name - prints a name
   * @name: the name
   * @f: function
+  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
