2198224310ba3ad48363fb72d9f95065b210a7d2
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-14 13:55:25 +0300

    second task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print the numbers you give it
+ * @separator: the separator between the numbers
+ * @n: the number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pr;
+	int i = n, x;
+
+	if (!n)
+		return;
+
+	va_start(pr, n);
+	while (i--)
+	{
+		x = va_arg(pr, int);
+		printf("%d%s", x, i ? (separator ? separator : "") : "\n");
+	}
+	va_end(pr);
+}
+
