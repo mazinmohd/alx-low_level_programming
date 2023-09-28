5c94ef335d0dbfefddfe9e85ccd91047a464ac8a
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-28 17:50:15 +0300

    task 0 convert bin to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b)
 	{
-		if (*b != '1' && *b != '1')
+		if (*b != '1' && *b != '0')
 			return (0);
 		cvt = cvt * 2 + (*b++ - '0');
 	}
