7835c066fbdc92303fedd22b7d831588665ca180
mazinmohd <mazenomermazen2002@gmail.com>
2023-10-02 13:38:57 +0300

    fix the betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (;text_content[r_num]; r_num++)
+		for (; text_content[r_num]; r_num++)
 			;
 		w_num = write(file, text_content, r_num);
 		if (w_num == -1)
 			return (-1);
-		
+
 	}
 	close(file);
 	return (1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,13 +13,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	file = open(filename, O_APPEND | O_WRONLY);
-	
+
 	if (file == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		for (;text_content[r_num] != '\0'; r_num++)
+		for (; text_content[r_num] != '\0'; r_num++)
 			;
 
 		w_num = write(file, text_content, r_num);
