779167a7deb880a0b504c4fc452f894399a411e5
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-09 12:38:11 +0300

    third task

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,6 +29,6 @@ size_t print_dlistint(const dlistint_t *h);
 
 size_t dlistint_len(const dlistint_t *h);
 
-dlistint_t *add_nodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
