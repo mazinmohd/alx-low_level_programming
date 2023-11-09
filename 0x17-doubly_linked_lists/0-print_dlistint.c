544359e870660ab7f4eb73ba2895a3ba1fc416c9
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-09 12:21:09 +0300

    first task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,9 +7,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	unsigned int i;
+	int i = 0;
 
-	for (i = 0; h->next != NULL; i++)
+	if (h == NULL)
+		return (i);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	for (; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
