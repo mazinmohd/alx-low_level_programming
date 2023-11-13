677c57b24532d69e7e08b794e5ad108a20779bf7
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-13 21:55:01 +0300

    la

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/mazen_omer/raw/main/alx-low_level_programming/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/mazen_omer/raw/master/alx-low_level_programming/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
