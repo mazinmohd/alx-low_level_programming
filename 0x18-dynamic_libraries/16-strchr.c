1872575a03438ca15049e37f82ff39997a28160f
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-13 07:25:41 +0300

    task num 0x18

---
+++ 0x18-dynamic_libraries/16-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ * Return: a pointer to location
+ */
+char *_strchr(char *s, char c)
+{
+		int a;
+
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+task num 0x18 in C which is about dynamic library
